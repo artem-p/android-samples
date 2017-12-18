2d4d61f31b1418ea8dfdb0a0a891429064e2d0d3
artem-p <artempugachev1@gmail.com>
2017-12-18 13:23:37 +0300

    init

---
+++ RecyclerViewExample/app/src/main/java/ru/artempugachev/recyclerviewexample/MainActivity.java
@@ -0,0 +1,16 @@
+package ru.artempugachev.recyclerviewexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Simple Recycler View example
+ * */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
